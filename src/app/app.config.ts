2d230d524f423463247d045ee83b0a85e05d8ed5
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-04-14 19:16:34 -0500

    Se realiza configuracion inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
