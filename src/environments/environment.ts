02ec85f1e40c79c38572be826cd7142ba1c63d8d
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-05-12 21:13:37 -0500

    Se ajusta la url del ingress

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,15 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-// const baseUrlRegistro = 'http://k8s-ekssport-sportapp-a5d22e537b-1623025658.us-east-1.elb.amazonaws.com/registro-usuarios';
-// const baseUrlGestorUsuarios = 'http://k8s-ekssport-sportapp-a5d22e537b-1623025658.us-east-1.elb.amazonaws.com/gestor-usuarios';
-// const baseUrlGestorSesiones = 'http://k8s-ekssport-sportapp-a5d22e537b-1623025658.us-east-1.elb.amazonaws.com/gestor-sesion-deportiva';
-// const baseUrlGestionProductosServicios = 'http://k8s-ekssport-sportapp-a5d22e537b-1623025658.us-east-1.elb.amazonaws.com/gestor-productos-servicios';
-const baseUrlRegistro = 'http://127.0.0.1:3001/registro-usuarios';
-const baseUrlGestorUsuarios = 'http://127.0.0.1:3001/gestor-usuarios';
-const baseUrlGestorSesiones = 'http://127.0.0.1:3005/gestor-sesion-deportiva';
-const baseUrlGestionProductosServicios = 'http://127.0.0.1:3006/gestor-productos-servicios';
+const URL_INGRES = 'http://k8s-ekssport-sportapp-a5d22e537b-1623025658.us-east-1.elb.amazonaws.com'
+const baseUrlRegistro = URL_INGRES+'/registro-usuarios';
+const baseUrlGestorUsuarios = URL_INGRES+'/gestor-usuarios';
+const baseUrlGestorSesiones = URL_INGRES+'/gestor-sesion-deportiva';
+const baseUrlGestionProductosServicios = URL_INGRES+'/gestor-productos-servicios';
+// const baseUrlRegistro = 'http://127.0.0.1:3001/registro-usuarios';
+// const baseUrlGestorUsuarios = 'http://127.0.0.1:3001/gestor-usuarios';
+// const baseUrlGestorSesiones = 'http://127.0.0.1:3005/gestor-sesion-deportiva';
+// const baseUrlGestionProductosServicios = 'http://127.0.0.1:3006/gestor-productos-servicios';
 
 export const environment = {
   production: false,
