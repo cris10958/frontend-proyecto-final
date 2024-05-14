fcc517374e5a9e093a73a4f4fc543c935ce8db0f
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-05-13 20:17:42 -0500

    Se realiza avance

--- src/app/detalle-venta.component.ts
+++ src/app/detalle-venta.component.ts
@@ -5,12 +5,70 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      detalle-venta works!
-    </p>
+    <div class="row fd-color p-4 shadow rounded pt-5">
+      <div class="col-12 text-start">
+        <h1 class="small">Comprador 1</h1>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Nombre</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          Juanito Perez
+        </h6>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Identificación</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          11111
+        </h6>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Dirección</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          Carrera Siempre Viva 123
+        </h6>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Teléfono</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          11111
+        </h6>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Metódo de pago</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          Tarjeta
+        </h6>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Estado de pago</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          Pago exitoso
+        </h6>
+      </div>
+      <div class="col-12 text-start pt-3">
+        <h1 class="small">Estado de entrega / prestación</h1>
+      </div>
+      <div class="col-12">
+        <h6 class="small color-letra-gray-600">
+          Pendiente
+        </h6>
+      </div>
+    </div>
+
   `,
   styles: ``
 })
 export class DetalleVentaComponent {
-
 }
